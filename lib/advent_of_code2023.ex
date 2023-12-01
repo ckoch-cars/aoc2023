a9090f8f7f6f6c2721da0ad60e119822847c2caa
Christian Koch <54853932+ckoch-cars@users.noreply.github.com>
2023-12-01 14:35:12 -0600

    day 1 part 2 solution
    missing edge cases in examples
    
    Not cool advent of code, not cool

--- lib/advent_of_code2023.ex
+++ lib/advent_of_code2023.ex
@@ -29,4 +29,44 @@ defmodule AdventOfCode2023 do
       acc + String.to_integer(i0 <> i1)
     end)
   end
+
+  def one_two(input) do
+    input
+    |> String.split("\n")
+    |> Enum.reduce(0, fn
+      nil, acc -> acc
+      "", acc -> acc
+      row, acc ->
+        values = Regex.scan(~r/one|two|three|four|five|six|seven|eight|nine|\d/, row)
+        i0 = values |> Enum.at(0) |> int_string_to_int()
+
+        rev_row = String.reverse(row)
+        values = Regex.scan(~r/enin|thgie|neves|xis|evif|ruof|eerht|owt|eno|\d/, rev_row)
+
+        i1 = values
+        |> Enum.at(0)
+        |> then(fn [val] -> [String.reverse(val)] end)
+        |> int_string_to_int()
+
+      acc + String.to_integer(i0 <> i1)
+    end)
+  end
+
+  @int_strings ~w(one two three four five six seven eight nine)
+  def int_string_to_int([int]) when int in @int_strings do
+    case int do
+      "one" -> "1"
+      "two" -> "2"
+      "three" -> "3"
+      "four" -> "4"
+      "five" -> "5"
+      "six" -> "6"
+      "seven" -> "7"
+      "eight" -> "8"
+      "nine" -> "9"
+    end
+  end
+
+  def int_string_to_int([int]), do: int
+
 end
--- test/advent_of_code2023_test.exs
+++ test/advent_of_code2023_test.exs
@@ -9,4 +9,31 @@ defmodule AdventOfCode2023Test do
   test "one" do
     assert AdventOfCode2023.one() == 54390
   end
+
+  test "one_two test" do
+    input = """
+    two1nine
+    eightwothree
+    abcone2threexyz
+    xtwone3four
+    4nineeightseven2
+    zoneight234
+    7pqrstsixteen
+    """
+    assert AdventOfCode2023.one_two(input) == 281
+  end
+
+  test "one_two edge" do
+    input = """
+    eighthree
+    sevenine
+    """
+    assert AdventOfCode2023.one_two(input) == 83 + 79
+  end
+
+  test "one_two full" do
+    input1 = File.read!("lib/fixtures/input1.txt")
+
+    assert AdventOfCode2023.one_two(input1) == 54277
+  end
 end
